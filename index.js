d586cc9aaca3e0d4720a3661da3cfe46d3c60e75
wgarden <westgarden@gmail.com>
2017-10-30 13:56:53 +0800

    recovery to the original index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-    redisURL: 'redis://localhost:59689'
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
